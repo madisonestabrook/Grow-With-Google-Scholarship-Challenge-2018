15f81a8ff363a8b6918032aa7950df4f9653e487
Madison Estabrook <madison.estabrook@gmail.com>
2018-04-09 17:40:17 -0400

    Create BuildingTheCrew.js

---
+++ Lesson15/BuildingTheCrew.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+console.log(crew);
