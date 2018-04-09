6ec57702f82c41bdf1d6b5e80ff84fd68dac7cd9
Madison Estabrook <madison.estabrook@gmail.com>
2018-04-09 17:38:36 -0400

    Create InlineFunctions.js

---
+++ Lesson14/InlineFunctions.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+// don't change this code
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+ var myString = "happy"; 
+ var ha = "ha"; 
+ for( var i = 0; i <=2 ; i++ ) {
+     return emotions + "!"; 
+ }
