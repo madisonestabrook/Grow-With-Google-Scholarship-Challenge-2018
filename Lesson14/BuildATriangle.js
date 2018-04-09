58c596644b52c71abd5b90e989af5c4c7f83a139
Madison Estabrook <madison.estabrook@gmail.com>
2018-04-09 17:37:21 -0400

    Create BuildATriangle.js

---
+++ Lesson14/BuildATriangle.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code
+function buildTriangle(length) { 
+    var triangle = ""; 
+  for(i = 1; i <= length; i++) { 
+      triangle += makeLine(i); 
+  }
+ return triangle; 
+}
+console.log(buildTriangle(10));
