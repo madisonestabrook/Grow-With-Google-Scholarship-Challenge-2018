eab0574827edb2cba12c68c2a8ed40d6fe7ff5ea
Madison Estabrook <madison.estabrook@gmail.com>
2018-04-09 17:40:58 -0400

    Create ThePriceIsRight.js

---
+++ Lesson15/ThePriceIsRight.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+prices[0] = 3.14; 
+prices[2] = 50.00; 
+prices[6] = 1.00; 
+console.log(prices);
