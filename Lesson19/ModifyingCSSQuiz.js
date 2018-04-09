ecef09232c333c52b2108f04fb494e2702d2abe6
Madison Estabrook <madison.estabrook@gmail.com>
2018-04-09 17:49:33 -0400

    Create ModifyingCSSQuiz.js

---
+++ Lesson19/ModifyingCSSQuiz.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, change the font-size of all the article-items to 20px!
+
+You must use jQuery's css() method!
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = $('.articles').css("fontSize","20px");
